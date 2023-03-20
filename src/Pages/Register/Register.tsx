873a34bf86800a538cab84c18e4e2d5893360287
mb-tousif <meta.block.tousif@gmail.com>
2023-03-20 10:10:13 +0600

    Registration field updated

--- src/Pages/Register/Register.tsx
+++ src/Pages/Register/Register.tsx
@@ -61,6 +61,36 @@ export default function Register() {
                   placeholder="Email"
                 />
               </div>
+              <div className="sm:flex justify-between">
+              <div className="mb-4">
+                <label
+                  className="block mb-2 text-sm font-bold text-gray-700"
+                  htmlFor="DOB"
+                >
+                  DOB
+                </label>
+                <input
+                  className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  id="DOB"
+                  type="date"
+                  placeholder="Enter Your DOB"
+                />
+              </div>
+              <div className="mb-6">
+                  <label className="inline-block mb-2 text-sm font-bold text-gray-700">
+                    User Role
+                  </label>
+                  <select
+                    className="w-full h-8 text-center bg-[#666666e7] text-gray-50 rounded-lg text-sm"
+                  >
+                    <option value="">--Select User--</option>
+                    <option value="A/C Holder">A/C Holder</option>
+                    <option value="Cashier">Cashier</option>
+                    <option value="Admin">Admin</option>
+                    <option value="Manager">Manager</option>
+                  </select>
+              </div>
+              </div>
               <div className="mb-4 md:flex md:justify-between">
                 <div className="mb-4 md:mr-2 md:mb-0">
                   <label
