68c132e10ba0b111fbd6d9e5b52e678ec5c2a7a2
mb-tousif <meta.block.tousif@gmail.com>
2023-04-01 14:55:30 +0600

    404 page updated

--- src/Components/Page404.tsx
+++ src/Components/Page404.tsx
@@ -4,12 +4,12 @@ const Page404 = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="text-center p-7">
+    <div className="text-center p-7 bg-slate-400 h-screen">
       <div className="my-auto sm:mt-20">
-        <h1 className="text-2xl">Uh oh! We’ve got a problem.</h1>
-      <div className="flex justify-center">
+        <h1 className="text-2xl mb-4 text-red-600">Oops! We’ve got a problem.</h1>
+      <div className="flex justify-center text-gray-50">
         <button
-          className="w-32 mr-2 mb-2 flex items-center justify-center bg-gray-200 text-gray-600 rounded-md shadow-md focus:outline-none"
+          className="w-32 mr-2 mb-2 p-1 bg-green-500 flex items-center justify-center rounded-md shadow-md focus:outline-none"
           onClick={() => navigate(-1)}
         >
           <i className='bx bx-arrow-back w-5'></i>
@@ -17,7 +17,7 @@ const Page404 = () => {
         </button>
         <Link
           to="/"
-          className="w-32 mb-2 flex items-center justify-center bg-gray-200 text-gray-600 rounded-md shadow-md focus:outline-none"
+          className="w-32 mb-2 flex items-center p-1 justify-center bg-green-500 rounded-md shadow-md focus:outline-none"
         >
           <i className='bx bxs-home w-5'></i>
           <span>Go home</span>
