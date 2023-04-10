752f9e111184b748eda533979f6c10dfe0cd14ea
mb-tousif <meta.block.tousif@gmail.com>
2023-04-10 23:54:34 +0600

    nav responsive done

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,27 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { GrTwitter } from "react-icons/gr";
-import { AiOutlineClose, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineYoutube } from "react-icons/ai";
-import '../Pages/Home/home.css'
+import {
+  AiOutlineClose,
+  AiOutlineInstagram,
+  AiOutlineLinkedin,
+  AiOutlineYoutube,
+} from "react-icons/ai";
+import "../Pages/Home/home.css";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
   const controlNavbar = () => {
     if (window.scrollY > 100) {
-      setShow(true)
+      setShow(true);
     } else {
-      setShow(false)
+      setShow(false);
     }
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener('scroll', controlNavbar)
+    window.addEventListener("scroll", controlNavbar);
     return () => {
-      window.removeEventListener('scroll', controlNavbar)
-    }
-  }, [])
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, []);
 
   const [hoverHide, setHoverHide] = useState(false);
   const handleMouseEnter = () => {
@@ -34,31 +39,46 @@ export default function Navbar() {
 
   return (
     <nav className="position">
-      <div className="mx-auto container">
+      <div className="mx-auto hidden sm:block">
         {/* nav one  */}
-        <div className={`nav-1 lg:justify-between sm:hidden lg:flex ${show && 'nav-hidden'}`}>
-          <div className='nav-icon-1 '>
-            <div className='flex'>
-              <AiOutlineInstagram className='text-white ml-3 pr-3 w-7 h-7 font-bold'></AiOutlineInstagram>
-              <GrTwitter className='text-white pr-3 w-7 ml-3 h-7 font-bold '></GrTwitter>
-              <AiOutlineLinkedin className='text-white pr-3 ml-3 w-7 h-7 font-bold ' ></AiOutlineLinkedin>
-              <AiOutlineYoutube className='text-white pr-3 ml-3 w-7 h-7 font-bold ' ></AiOutlineYoutube>
+        <div
+          className={`nav-1 lg:justify-between sm:hidden lg:flex ${
+            show && "nav-hidden"
+          }`}
+        >
+          <div className="nav-icon-1 ">
+            <div className="flex">
+              <AiOutlineInstagram className="text-white ml-3 pr-3 w-7 h-7 font-bold"></AiOutlineInstagram>
+              <GrTwitter className="text-white pr-3 w-7 ml-3 h-7 font-bold "></GrTwitter>
+              <AiOutlineLinkedin className="text-white pr-3 ml-3 w-7 h-7 font-bold "></AiOutlineLinkedin>
+              <AiOutlineYoutube className="text-white pr-3 ml-3 w-7 h-7 font-bold "></AiOutlineYoutube>
             </div>
           </div>
-          <div className='nav-menu-1'>
-            <div className='text-white text-[13px] flex'>
-              <h2 className='pl-3'>ONLINE BANKING</h2>
-              <h2 className='pl-3'>FORMS</h2>
-              <h2 className='pl-3 mr-3'>CONTACT</h2>
+          <div className="nav-menu-1">
+            <div className="text-white text-[13px] flex">
+              <h2 className="pl-3">ONLINE BANKING</h2>
+              <h2 className="pl-3">FORMS</h2>
+              <h2 className="pl-3 mr-3">CONTACT</h2>
             </div>
           </div>
         </div>
         {/* nav two  */}
-        <div className={`nav-2-fixed ${show && 'nav-2-top'} ${!show && 'nav-2-animation'}`}>
-          <div onMouseEnter={handleMouseLeave} className={`flex nav-2 ${hoverHide && 'nav-Hide'} 
+        <div
+          className={`nav-2-fixed ${show && "nav-2-top"} ${
+            !show && "nav-2-animation"
+          }`}
+        >
+          <div
+            onMouseEnter={handleMouseLeave}
+            className={`flex nav-2 ${hoverHide && "nav-Hide"} 
           bg-[#ffffffcc]  nav-border-left nav-border-right
-           ${show && 'nav-border-left-none nav-border-right-none'}`}>
-            <div className={`style-1 flex-none w-[4%] py-4  nav-border-left ${show && 'nav-border-left-none'}`}></div>
+           ${show && "nav-border-left-none nav-border-right-none"}`}
+          >
+            <div
+              className={`style-1 flex-none w-[4%] py-4  nav-border-left ${
+                show && "nav-border-left-none"
+              }`}
+            ></div>
             <div className="logo flex-1 w-[24%] py-4 pl-4 ">
               <h1 className=" text-[#03A776] px-3  justify-end rounded-md text-lg md:text-xl font-medium">
                 𝕯𝖊𝖛𝖊𝖑𝖔𝖕𝖊𝖗 𝕭𝖆𝖓𝖐 𝕷𝖙𝖉
@@ -104,19 +124,28 @@ export default function Navbar() {
                 </Link>
               </div>
             </div>
-            <div className={`style-2 flex-none ${hoverHide && 'nav-Hide'} w-[4%] py-4 nav-border-right ${show && 'nav-border-right-none'}`}></div>
+            <div
+              className={`style-2 flex-none ${
+                hoverHide && "nav-Hide"
+              } w-[4%] py-4 nav-border-right ${
+                show && "nav-border-right-none"
+              }`}
+            ></div>
           </div>
-          <div onClick={() => setOpen(!open)} className="-mr-2 flex sm:hidden">
-            {open ? (
-              <AiOutlineClose className="w-8 h-8 text-gray-50" />
-            ) : (
-              <GiHamburgerMenu className="w-8 h-8 text-gray-50" />
-            )}
-          </div>
-          <div
+        </div>
+      </div>
+      <div className="bg-[#0978]">
+      <div onClick={() => setOpen(!open)} className="-mr-2 flex sm:hidden">
+        {open ? (
+          <AiOutlineClose className="w-8 h-8 text-gray-50" />
+        ) : (
+          <GiHamburgerMenu className="w-8 h-8 text-gray-50" />
+        )}
+      </div>
+      <div
         className={`${
           open ? "block" : "hidden"
-        } absolute z-20 bg-[#232274f9] w-full md:hidden opacity-90`}
+        } absolute z-20 w-full md:hidden bg-[#0978] opacity-90`}
       >
         <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 translate ease-in-out duration-300">
           <Link
@@ -156,9 +185,8 @@ export default function Navbar() {
             Login
           </Link>
         </div>
-        </div>
-        </div>
+      </div>
       </div>
     </nav>
-  )
+  );
 }
